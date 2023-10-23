be2d359dcfebf5944354e21cb7347fdc5ff67dea
Arbel Arad <Arbel@spacetime.technology>
2023-10-23 17:00:57 +0000

    fix more mount mistakes

--- btrfs_subvol_mount.sh
+++ btrfs_subvol_mount.sh
@@ -1,12 +1,14 @@
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@ /dev/mapper/cryptroot /mnt
-mkdir -p /mnt/{efi,home,var/tmp,var/log,var/account,usr/share,tmp,.snapshots,dev/shm}
-mount -o nodev,nosuid,noexec /dev/EFI /mnt/efi
+mkdir -p /mnt/{efi,home,tmp,usr,dev/shm,var,.snapshots}
+mount -o nodev,nosuid,noexec /dev/disk/by-label/EFI /mnt/efi
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@home,nodev,nosuid /dev/mapper/cryptroot /mnt/home
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@tmp,nodev,nosuid,noexec /dev/mapper/cryptroot /mnt/tmp
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@usr,nodev /dev/mapper/cryptroot /mnt/usr
+mkdir -p /mnt/usr/share
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@usr_share,nodev,nosuid /dev/mapper/cryptroot /mnt/usr/share
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@dev_shm,nodev,nosuid,noexec /dev/mapper/cryptroot /mnt/dev/shm
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@var,nodev /dev/mapper/cryptroot /mnt/var
+mkdir -p /mnt/var/{tmp,log,account}
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@var_tmp,nodev,nosuid,noexec /dev/mapper/cryptroot /mnt/var/tmp
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@var_log,nodev,nosuid,noexec /dev/mapper/cryptroot /mnt/var/log
 mount -o defaults,compress=zstd:1,discard=async,ssd,subvol=@var_account,nodev,nosuid,noexec /dev/mapper/cryptroot /mnt/var/account
