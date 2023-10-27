c7a3ed52c760f1500bb7cbfc29c6086916cf5bf3
Arbel Arad <Arbel@spacetime.technology>
2023-10-27 20:25:31 +0000

    hopefully add_user.sh should work now

--- add_user.sh
+++ add_user.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 fido_dev=$(systemd-cryptenroll --fido2-device=list | grep '^/dev/hidraw[0-9]\+' -o)
 read -p "username: " username
-useradd -m -G wheel -g users -s /bin/zsh ${username}
-mkdir -p /home/{${username},${username}.crypt}
-chown -R ${username}:users /home/{${username},${username}.crypt}
-sudo gocryptfs -init --fido2=${fido_dev} /home/$(whoami).crypt
-sudo gocryptfs --fido2=${fido_dev} /home/$(whoami).crypt ~ -nonempty -q
-passwd ${username}
+sudo useradd -m -G wheel -g users -s /bin/zsh ${username}
+sudo mkdir -p /home/{${username},${username}.crypt}
+sudo gocryptfs -init --fido2=${fido_dev} /home/${username}.crypt
+sudo gocryptfs --fido2=${fido_dev} /home/${username}.crypt /home/${username} -nonempty -q
+sudo chown -R ${username}:users /home/{${username},${username}.crypt}
+sudo passwd ${username}
