7623eb832756939834bcca5ae6326bcbc1061a20
Arbel Arad <Arbel@spacetime.technology>
2023-10-27 20:07:57 +0000

    add_user su -c for creating crypt dir

--- add_user.sh
+++ add_user.sh
@@ -2,7 +2,7 @@
 read -p "username: " username
 useradd -m -G wheel -g users -s /bin/zsh ${username}
 mkdir -p /home/{${username},${username}.crypt}
-gocryptfs -init --fido2=$fido_dev /home/${user}.crypt
 chown -R ${username}:users /home/{${username},${username}.crypt}
+sudo su ${username} -c 'gocryptfs -init --fido2=$fido_dev /home/$(whoami).crypt'
+sudo su ${username} -c 'gocryptfs --fido2=$fido_dev /home/$(whoami).crypt ~ -nonempty -q'
 passwd ${username}
-su ${username} -c 'username=$(whoami) && gocryptfs --fido2=$fido_dev /home/${username}.crypt ~ -nonempty -q'
