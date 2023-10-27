40e2488cb41d7d318ca69842a89e0995584145ea
Arbel Arad <Arbel@spacetime.technology>
2023-10-27 20:11:43 +0000

    fixed fido2=device for add_user

--- add_user.sh
+++ add_user.sh
@@ -3,6 +3,6 @@ read -p "username: " username
 useradd -m -G wheel -g users -s /bin/zsh ${username}
 mkdir -p /home/{${username},${username}.crypt}
 chown -R ${username}:users /home/{${username},${username}.crypt}
-sudo su ${username} -c 'gocryptfs -init --fido2=$fido_dev /home/$(whoami).crypt'
-sudo su ${username} -c 'gocryptfs --fido2=$fido_dev /home/$(whoami).crypt ~ -nonempty -q'
+sudo su ${username} -c 'gocryptfs -init --fido2=$(systemd-cryptenroll --fido2-device=list | grep '^/dev/hidraw[0-9]\+' -o) /home/$(whoami).crypt'
+sudo su ${username} -c 'gocryptfs --fido2=$(systemd-cryptenroll --fido2-device=list | grep '^/dev/hidraw[0-9]\+' -o) /home/$(whoami).crypt ~ -nonempty -q'
 passwd ${username}
