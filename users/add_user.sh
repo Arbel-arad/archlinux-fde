f51057c94e864d8c323ece4782b6193eb85ccfe7
Arbel Arad <Arbel@spacetime.technology>
2023-10-28 09:28:05 +0000

    reordered add_user

--- users/add_user.sh
+++ users/add_user.sh
@@ -5,7 +5,7 @@ sudo useradd -m -G wheel -g users -s /bin/zsh ${username}
 sudo mkdir -p /home/{${username},${username}.crypt}
 sudo gocryptfs -init --fido2=${fido_dev} /home/${username}.crypt
 sudo cp -i -r .config /home/${username}
-sudo chmod +x /home/${username}/.config/hypr/scripts/*.sh
+sudo chmod +x /home/${username}/.config/hypr/scripts/mount_home.sh
 sudo gocryptfs --fido2=${fido_dev} /home/${username}.crypt /home/${username} -nonempty -q
 #copy user files now
 sudo umount /home/${username}
