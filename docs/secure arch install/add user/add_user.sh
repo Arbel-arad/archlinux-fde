2c5fd0260b038c74cbd56b3416b3ca7c51e889a1
Arbel Arad <Arbel@spacetime.technology>
2023-12-08 22:45:40 +0200

    added network connection step

---
+++ docs/secure arch install/add user/add_user.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+fido_dev=$(systemd-cryptenroll --fido2-device=list | grep '^/dev/hidraw[0-9]\+' -o)
+read -p "username: " username
+sudo useradd -m -G wheel -g users -s /bin/zsh ${username}
+sudo mkdir -p /home/{${username},${username}.crypt}
+sudo gocryptfs -init --fido2=${fido_dev} /home/${username}.crypt
+sudo cp -i -r .config /home/${username}
+sudo chmod +x /home/${username}/.config/hypr/scripts/mount_home.sh
+sudo gocryptfs --fido2=${fido_dev} /home/${username}.crypt /home/${username} -nonempty -q
+sudo cp -i -r .config /home/${username}
+#copy user files now
+sudo umount /home/${username}
+sudo chown -R ${username}:users /home/{${username},${username}.crypt}
+sudo passwd ${username}
